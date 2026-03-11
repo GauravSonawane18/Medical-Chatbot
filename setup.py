dac9fe396d745ac512bcc26ebaf96e44cd6af03b
Gaurav <gaurav.sonawane.08080@gmail.com>
2026-03-11 18:24:45 +0530

    requirements installed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Medical-Chatbot",
+    version="0.1.0",
+    author="Gaurav",
+    author_email="gauravsonawane25.26@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
